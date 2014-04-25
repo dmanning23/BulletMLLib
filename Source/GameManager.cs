54677f730c68d3f12378d8ffb953d0af5437f8bf
Daniel Manning <daniel.manning@demandmedia.com>
2014-04-25 17:20:57 -0500

    master: a-0 need to move some stuff around

--- Source/GameManager.cs
+++ Source/GameManager.cs
@@ -6,6 +6,10 @@ namespace BulletMLLib
 	/// </summary>
 	public static class GameManager
 	{
+		//TODO: get rid of this class and move game difficulty in to bullet manager
+
+		//TODO: bullet should store the difficulty when they are fired
+
 		/// <summary>
 		/// callback method to get the game difficulty.
 		/// You need to set this at the start of the game
